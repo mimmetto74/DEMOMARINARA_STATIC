88160f4aa2d08352316df3d5f3fcea51a2f0a390
cosimo <cosimo.pignatelli@email.it>
2025-10-09 20:05:34 +0200

    Add files via upload

--- pv_forecast_all_in_one.py
+++ pv_forecast_all_in_one.py
@@ -537,7 +537,7 @@ with tab5:
 
     real_file = st.file_uploader("CSV produzione reale", type=["csv"])
     use_session_pred = st.toggle("Usa previsioni calcolate nel tab Previsioni", value=True)
-    tz_local = st.toggle("Usa fuso orario Europe/Rome (altrimenti UTC)", value=True)\", value=True)
+    tz_local = st.toggle("Usa fuso orario Europe/Rome (altrimenti UTC)", value=True)", value=True)\", value=True)
     tz = \"Europe/Rome\" if tz_local else \"UTC\"
     pred_file = None if use_session_pred else st.file_uploader("CSV previsioni (opzionale)", type=["csv"], help="Colonna kWh_15m o kWh_curve (oppure kW_inst)")
 
